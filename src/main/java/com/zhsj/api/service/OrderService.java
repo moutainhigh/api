2a8221e7e06cf8b609a593d4e62829904e1628ad
lcg12017 <lcg12017@163.com>
2017-06-27 22:48:04 +0800

    modi

--- src/main/java/com/zhsj/api/service/OrderService.java
+++ src/main/java/com/zhsj/api/service/OrderService.java
@@ -285,33 +285,33 @@ public class OrderService {
 				 bean.setDisplayWX(0);
 			 }else{
 				 bean.setDisplayWX(1);
-				 bean.setActualMoneyWX(wxMap.get("planMoney") == null?0:((BigDecimal)wxMap.get("planMoney")).doubleValue());
+				 bean.setPlanMoneyWX(wxMap.get("planMoney") == null?0:((BigDecimal)wxMap.get("planMoney")).doubleValue());
 				 bean.setActualMoneyWX(wxMap.get("actualMoney") == null?0:((BigDecimal)wxMap.get("actualMoney")).doubleValue());
-				 bean.setActualMoneyWX(wxMap.get("storeDisMoney") == null?0:((BigDecimal)wxMap.get("storeDisMoney")).doubleValue());
-				 bean.setActualMoneyWX(wxMap.get("orgDisMoney") == null?0:((BigDecimal)wxMap.get("orgDisMoney")).doubleValue());
-				 bean.setActualMoneyWX(wxMap.get("refundMoney") == null?0:((BigDecimal)wxMap.get("refundMoney")).doubleValue());
+				 bean.setStoreDisMoneyWX(wxMap.get("storeDisMoney") == null?0:((BigDecimal)wxMap.get("storeDisMoney")).doubleValue());
+				 bean.setOrgDisMoneyWX(wxMap.get("orgDisMoney") == null?0:((BigDecimal)wxMap.get("orgDisMoney")).doubleValue());
+				 bean.setRefundWX(wxMap.get("refundMoney") == null?0:((BigDecimal)wxMap.get("refundMoney")).doubleValue());
 			 }
 			
 			 if(aliMap == null){
 				 bean.setDisplayAli(0);
 			 }else{
 				 bean.setDisplayAli(1);
-				 bean.setActualMoneyAli(aliMap.get("planMoney") == null?0:((BigDecimal)aliMap.get("planMoney")).doubleValue());
+				 bean.setPlanMoneyAli(aliMap.get("planMoney") == null?0:((BigDecimal)aliMap.get("planMoney")).doubleValue());
 				 bean.setActualMoneyAli(aliMap.get("actualMoney") == null?0:((BigDecimal)aliMap.get("actualMoney")).doubleValue());
-				 bean.setActualMoneyAli(aliMap.get("storeDisMoney") == null?0:((BigDecimal)aliMap.get("storeDisMoney")).doubleValue());
-				 bean.setActualMoneyAli(aliMap.get("orgDisMoney") == null?0:((BigDecimal)aliMap.get("orgDisMoney")).doubleValue());
-				 bean.setActualMoneyAli(aliMap.get("refundMoney") == null?0:((BigDecimal)aliMap.get("refundMoney")).doubleValue());
+				 bean.setStoreDisMoneyAli(aliMap.get("storeDisMoney") == null?0:((BigDecimal)aliMap.get("storeDisMoney")).doubleValue());
+				 bean.setOrgDisMoneyAli(aliMap.get("orgDisMoney") == null?0:((BigDecimal)aliMap.get("orgDisMoney")).doubleValue());
+				 bean.setRefundAli(aliMap.get("refundMoney") == null?0:((BigDecimal)aliMap.get("refundMoney")).doubleValue());
 			 }
 			 
 			 if(unMap == null){
 				 bean.setDisplayUCard(0);
 			 }else{
-				 bean.setDisplayWX(1);
-				 bean.setActualMoneyUCard(unMap.get("planMoney") == null?0:((BigDecimal)unMap.get("planMoney")).doubleValue());
+				 bean.setDisplayUCard(1);
+				 bean.setPlanMoneyUCard(unMap.get("planMoney") == null?0:((BigDecimal)unMap.get("planMoney")).doubleValue());
 				 bean.setActualMoneyUCard(unMap.get("actualMoney") == null?0:((BigDecimal)unMap.get("actualMoney")).doubleValue());
-				 bean.setActualMoneyUCard(unMap.get("storeDisMoney") == null?0:((BigDecimal)unMap.get("storeDisMoney")).doubleValue());
-				 bean.setActualMoneyUCard(unMap.get("orgDisMoney") == null?0:((BigDecimal)unMap.get("orgDisMoney")).doubleValue());
-				 bean.setActualMoneyUCard(unMap.get("refundMoney") == null?0:((BigDecimal)unMap.get("refundMoney")).doubleValue());
+				 bean.setStoreDisMoneyUCard(unMap.get("storeDisMoney") == null?0:((BigDecimal)unMap.get("storeDisMoney")).doubleValue());
+				 bean.setOrgDisMoneyUCard(unMap.get("orgDisMoney") == null?0:((BigDecimal)unMap.get("orgDisMoney")).doubleValue());
+				 bean.setRefundUCard(unMap.get("refundMoney") == null?0:((BigDecimal)unMap.get("refundMoney")).doubleValue());
 
 			 }
 			 
