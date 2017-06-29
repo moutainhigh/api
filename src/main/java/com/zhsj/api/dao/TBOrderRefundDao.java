a41dd23efac9e61a18dcf0b89dbf139c818d8f55
lcg12017 <lcg12017@163.com>
2017-06-29 13:33:44 +0800

    dd

--- src/main/java/com/zhsj/api/dao/TBOrderRefundDao.java
+++ src/main/java/com/zhsj/api/dao/TBOrderRefundDao.java
@@ -23,6 +23,7 @@ public interface TBOrderRefundDao {
 	OrderRefundBean getByRefundNo(@Param("refundNo")String refundNo);
 	
 	int updateStatusAndOrderNo(@Param("reOrderNo")String reOrderNo,
-			@Param("status")int status);
+							   @Param("preOrderNo")String preOrderNo,
+							   @Param("status")int status);
 
 }
